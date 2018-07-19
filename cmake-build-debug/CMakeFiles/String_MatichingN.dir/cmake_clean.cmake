0d3319a5aa053658c8c2d8c3cdbdbead8789c175
Iresh Dissanayaka <ireshmdev@gmail.com>
2018-07-19 10:42:40 +0530

    finished algorithm

---
+++ cmake-build-debug/CMakeFiles/String_MatichingN.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/String_MatichingN.dir/main.c.obj"
+  "String_MatichingN.pdb"
+  "String_MatichingN.exe"
+  "String_MatichingN.exe.manifest"
+  "libString_MatichingN.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/String_MatichingN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+void findPattern(char * string, char * pattern);
 
 int main() {
-    printf("Hello, World!\n");
+    findPattern("fadasdasdsrsda", "da");
     return 0;
+}
+
+void findPattern(char * string, char * pattern) {
+    int n = strlen(string);
+    int m = strlen(pattern);
+
+    for (int i = 0; i <= n - m; ++i) {
+        int j;
+        for (j = 0; j < m; ++j) {
+            if(string[i + j] != pattern[j]) break;
+        }
+
+        if(j == m) printf("%s %d\n", "Pattern found at index:", i);
+    }
 }
